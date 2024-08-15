0d11e0e0564086f3a8c2145f390ad42c5cee52f4
Ezio <277341642@qq.com>
2024-08-15 15:26:40 +0800

    测试工作流配置

--- packages/apps/ics-server-api-gateway/user-gateway/src/app.service.ts
+++ packages/apps/ics-server-api-gateway/user-gateway/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello user-gateway! 1';
+    return 'Hello user-gateway! 2';
   }
 }
